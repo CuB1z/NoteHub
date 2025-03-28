3f46b4c74b2de0615d728cdbb36fa03adb3962e3
CuB1z <cub1zdev@gmail.com>
2025-03-28 21:27:23 +0100

    Refactor: Turn DynamicPathPage to render content using MarkdownRenderer

--- frontend/app/[...path]/page.tsx
+++ frontend/app/[...path]/page.tsx
@@ -2,22 +2,17 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { fetchData } from "@/lib/fetchData";
 import { ContentResponse } from "@/types/ContentResponse";
+import MarkdownRenderer from "@/components/MarkdownRenderer";
 
-export default async function DynamicPathPage({ params }: { params: { path: string[] } }) {
+export default async function DynamicPathPage(context: { params: { path: string[] } }) {
+    const params = await context.params;
     const session = await getServerSession(authOptions);
     const fullPath = params.path.join("/");
 
     const response = await fetchData<ContentResponse>(
         `http://localhost:5000/api/v1/repositories/${fullPath}`,
         session?.accessToken as string,
-    )
+    );
 
-    return (
-        <div>
-            <h1>Dynamic Path</h1>
-            <p>Current Path: {fullPath}</p>
-            <h2>File Content</h2>
-            <pre>{response?.content}</pre>
-        </div>
-    )
+    return <MarkdownRenderer markdown={response?.content || ""} />;
 }
