767b378995435b205b0b9b9fb8928ed889685b97
CuB1z <cub1zdev@gmail.com>
2025-03-28 22:42:43 +0100

    Refactor: Refactor routing to handle repository page

--- frontend/app/[name]/[repo]/[...path]/page.tsx
+++ frontend/app/[name]/[repo]/[...path]/page.tsx
@@ -1,13 +1,21 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { fetchData } from "@/lib/fetchData";
 import { ContentResponse } from "@/types/ContentResponse";
 import MarkdownRenderer from "@/components/MarkdownRenderer";
 
-export default async function DynamicPathPage(context: { params: { path: string[] } }) {
+interface PageProps {
+    params: {
+        name: string;
+        repo: string;
+        path: string[]
+    };
+};
+
+export default async function DynamicPathPage(context: PageProps) {
     const params = await context.params;
     const session = await getServerSession(authOptions);
-    const fullPath = params.path.join("/");
+    const fullPath = `${params.name}/${params.repo}/${params.path.join("/")}`;
 
     const response = await fetchData<ContentResponse>(
         `http://localhost:5000/api/v1/repositories/${fullPath}`,
