8d323b3a6014191a2554de8a14d5521f27c8d7a1
CuB1z <cub1zdev@gmail.com>
2025-03-29 10:17:20 +0100

    Feature: Enhance Markdown parsing with custom link handling and smooth scrolling in styles

--- frontend/lib/parseMarkdown.ts
+++ frontend/lib/parseMarkdown.ts
@@ -1,8 +1,34 @@
 import { marked } from "marked"
 import markedKatex from "marked-katex-extension";
+import { gfmHeadingId } from "marked-gfm-heading-id";
+
+interface MarkdownLink {
+    href: string;
+    text: string;
+}
 
 export function parseMarkdown(markdown: string): string {
+    const renderer = new marked.Renderer();
+
+    renderer.link = ({ href, text }: MarkdownLink) => {
+        if (href.startsWith("http")) {
+            return `<a href="${href}" target="_blank" rel="noopener noreferrer">${text}</a>`;
+        } else if (href.startsWith("#")) {
+            const newHref = href
+                .toLocaleLowerCase()
+                .replace(/ /g, "-")
+                .replace(/%20/g, "-")
+                .replace(/[()]/g, "");
+
+            return `<a href="${newHref}">${text}</a>`;
+        } else {
+            return `<a href="${href}">${text}</a>`;
+        }
+    }
+
     marked.use(markedKatex({ throwOnError: false }));
+    marked.use(gfmHeadingId());
+    marked.use({ renderer });
 
     return marked.parse(markdown, {
         async: false,
