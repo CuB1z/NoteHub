8e108abe24506a2cb45089d05c1c461e2da80db5
CuB1z <cub1zdev@gmail.com>
2025-04-06 12:22:32 +0200

    Feature: Enhance Hero section with scroll prompt and improved animation timing, update Footer styles for better text visibility

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,6 @@
 import styles from "@/styles/sections/Hero.module.css"
 import SearchBar from "@/components/SearchBar"
+import { Mouse } from "lucide-react"
 
 export default function Hero() {
     return(
@@ -7,6 +8,12 @@ export default function Hero() {
             <h1 className={styles.h1}>Your <em className={styles.em}>Knowledge Graph</em>, Anywhere</h1>
             <p className={styles.description}>Seamlessly render and navigate through your Obsidian vaults stored in GitHub repositories with our powerful viewer. No installation required.</p>
             <SearchBar />
+            <div className={styles.scroll}>
+                <span>Scroll to explore more</span>
+                <div className={styles.scrollIcon}>
+                    <Mouse />
+                </div>
+            </div>
         </div>
     )
 }
