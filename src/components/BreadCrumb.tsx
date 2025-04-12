9e523800888d46e3d6af1e1a19258030bc898a1d
CuB1z <cub1zdev@gmail.com>
2025-04-12 12:42:04 +0200

    Feature: Enhance BreadCrumb and Button components with improved styling and icon integration

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -1,6 +1,7 @@
 import styles from "@/styles/BreadCrumb.module.css";
 
 import { RepoOptions } from "@/services/RepositoryService";
+import { Home } from "lucide-react";
 import Button from "./buttons/Button";
 
 export default function BreadCrumb({ githubOwner, githubRepo, path }: RepoOptions) {
@@ -11,7 +12,7 @@ export default function BreadCrumb({ githubOwner, githubRepo, path }: RepoOption
                 variant="text"
                 isLink
                 href={`/${githubOwner}`}
-            />
+            ><Home /></Button>
             <span>{`${">"}`}</span>
             <Button
                 label={githubRepo}
