71d81ddf5f920da937675e9244f52a78ceca4787
CuB1z <cub1zdev@gmail.com>
2025-04-20 16:12:15 +0200

    Feature: Refactor Loader component to accept size prop and apply styles from Loader.module.css; remove inline styles from globals.css

--- src/components/skeletons/Loader.tsx
+++ src/components/skeletons/Loader.tsx
@@ -1,10 +1,15 @@
+import styles from "@/styles/Loader.module.css";
 
 import { Loader2Icon } from "lucide-react";
 
-export default function Loader() {
+export default function Loader({ size = 48 }: { size?: number }) {
     return (
-        <div>
-            <Loader2Icon className="loader" />
+        <div className={styles.container}>
+            <Loader2Icon
+                className={styles.loader}
+                style={{ width: size, height: size }}
+                color="var(--accent)"
+            />
         </div>
     );
 }
